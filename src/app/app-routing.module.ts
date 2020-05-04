093f83c394b9c6caca71bf43750086ceccf4aaf4
kunalsharma111 <sharmakunal315@gmail.com>
2020-05-04 14:07:16 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+  path: '',
+  redirectTo: '/pages/authentication',
+  pathMatch: 'full'
+  }, 
+  {
+  path: 'pages',
+  loadChildren: './pages/pages.module#PagesModule'
+  }, 
+  {
+  path: '**',
+  redirectTo: '/pages/authentication'
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/services/services.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormService } from './form/form.service';
+import { ChatService } from './chat/chat.service';
+import { AuthGuardService } from './auth-guard/auth-guard.service';
+import { SocketService } from './socket/socket.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    FormService,
+    ChatService,
+    AuthGuardService,
+    SocketService
+]
+})
+export class ServicesModule { 
+
+}
